b8b35be63066eaaa82ce8d60012c2b7bb74a1d0c
Rodolfo Gabriel <rodolfo.gabriel@hotmail.com.br>
2020-12-15 15:30:47 -0300

    Basic layout home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,21 @@
+import { Container } from "react-bootstrap";
+
+// components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 function App() {
-    return <div className="App">app</div>;
+    return (
+        <>
+            <Header />
+            <main className="py-3">
+                <Container>
+                    <h1>WELCOME TO THE SHOP</h1>
+                </Container>
+            </main>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
