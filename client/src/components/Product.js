b06c1c7aec3c821980dacc423ee1eebc87f66a93
Rodolfo Gabriel <rodolfo.gabriel@hotmail.com.br>
2020-12-15 17:00:40 -0300

    Created individual product page

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -15,10 +15,7 @@ const Product = ({ product }) => {
                     </Card.Title>
                 </Link>
                 <Card.Text as="div">
-                    <Rating
-                        value={product.rating}
-                        text={`${product.numReviews} reviews`}
-                    />
+                    <Rating value={product.rating} text={product.numReviews} />
                 </Card.Text>
 
                 <Card.Text as="h3">${product.price}</Card.Text>
