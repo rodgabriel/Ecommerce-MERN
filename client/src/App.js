1c92d8327deca1d2dc284d03c6af0df585a9ba9b
Rodolfo Gabriel <rodolfo.gabriel@hotmail.com.br>
2020-12-15 16:38:55 -0300

    Set up routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import { Container } from "react-bootstrap";
 
 // components
@@ -5,18 +7,20 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 //pages
 import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
     return (
-        <>
+        <Router>
             <Header />
             <main className="py-3">
                 <Container>
-                    <HomePage />
+                    <Route path="/" exact component={HomePage} />
+                    <Route path="/product/:id" component={ProductPage} />
                 </Container>
             </main>
             <Footer />
-        </>
+        </Router>
     );
 }
 
