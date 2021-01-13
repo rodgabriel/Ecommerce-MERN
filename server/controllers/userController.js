8bf57bd73645440714c889f96f647b6d3aff826a
Rodolfo <rodolfo.gabriel@hotmail.com.br>
2021-01-13 13:30:12 -0300

    Generate token and return in login res

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
 // POST api/users/login => Auth user & get token
 const authUser = asyncHandler(async (req, res) => {
@@ -13,7 +14,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
--- server/index.js
+++ server/index.js
@@ -4,17 +4,13 @@ import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 import productRoutes from "./routes/productRoutes.js";
-<<<<<<< HEAD
 import userRoutes from "./routes/userRoutes.js";
-=======
->>>>>>> 707884f9c09f0281d08654c12b07652870a97685
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
-<<<<<<< HEAD
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -23,13 +19,6 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
-=======
-app.get("/", (req, res) => {
-    res.send("API is running");
-});
-
-app.use("/api/products", productRoutes);
->>>>>>> 707884f9c09f0281d08654c12b07652870a97685
 
 app.use(notFound);
 
@@ -38,11 +27,6 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(
-<<<<<<< HEAD
   PORT,
   console.log(`Running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-=======
-    PORT,
-    console.log(`Running in ${process.env.NODE_ENV} mode on port ${PORT}`)
->>>>>>> 707884f9c09f0281d08654c12b07652870a97685
 );
