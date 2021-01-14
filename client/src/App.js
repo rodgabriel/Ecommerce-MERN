9a91266906d8a724b17318e53c820a8c43ffe91b
Rodolfo <rodolfo.gabriel@hotmail.com.br>
2021-01-14 12:28:13 -0300

    Added login page and form

--- client/src/App.js
+++ client/src/App.js
@@ -9,21 +9,23 @@ import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 import CartPage from "./pages/CartPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-    return (
-        <Router>
-            <Header />
-            <main className="py-3">
-                <Container>
-                    <Route path="/" exact component={HomePage} />
-                    <Route path="/product/:id" component={ProductPage} />
-                    <Route path="/cart/:id?" component={CartPage} />
-                </Container>
-            </main>
-            <Footer />
-        </Router>
-    );
+  return (
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path="/login" component={LoginPage} />
+          <Route path="/product/:id" component={ProductPage} />
+          <Route path="/cart/:id?" component={CartPage} />
+          <Route path="/" exact component={HomePage} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
