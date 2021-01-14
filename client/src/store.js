abfe62f69e671173aa7999fe07d847c0a850751b
Rodolfo <rodolfo.gabriel@hotmail.com.br>
2021-01-14 13:11:07 -0300

    Add user details profile action and reducer

--- client/src/store.js
+++ client/src/store.js
@@ -10,7 +10,11 @@ import {
 
 import { cartReducer } from "./reducers/cartReducers";
 
-import { userLoginReducer, userRegisterReducer } from "./reducers/userReducers";
+import {
+  userLoginReducer,
+  userRegisterReducer,
+  userDetailsReducer,
+} from "./reducers/userReducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -18,6 +22,7 @@ const reducer = combineReducers({
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
