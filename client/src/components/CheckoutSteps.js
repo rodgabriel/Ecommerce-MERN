87c247e62085cb585caa8c6a4200836d66d3c4b6
Rodolfo <rodolfo.gabriel@hotmail.com.br>
2021-01-17 11:35:10 -0300

    Add styled numbers to checkout steps

--- client/src/components/CheckoutSteps.js
+++ client/src/components/CheckoutSteps.js
@@ -7,37 +7,53 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
       <Nav.Item>
         {step1 ? (
           <LinkContainer to="/login">
-            <Nav.Link>Sign In</Nav.Link>
+            <Nav.Link>
+              <small className="mr-2">&#10102;</small> Sign In
+            </Nav.Link>
           </LinkContainer>
         ) : (
-          <Nav.Link disabled>Sign In</Nav.Link>
+          <Nav.Link disabled>
+            <span className="mr-2">&#10112;</span> Sign In
+          </Nav.Link>
         )}
       </Nav.Item>
       <Nav.Item>
         {step2 ? (
           <LinkContainer to="/shipping">
-            <Nav.Link>Shipping</Nav.Link>
+            <Nav.Link>
+              <small className="mr-2">&#10103;</small>Shipping
+            </Nav.Link>
           </LinkContainer>
         ) : (
-          <Nav.Link disabled>Shipping</Nav.Link>
+          <Nav.Link disabled>
+            <span className="mr-2">&#10113;</span>Shipping
+          </Nav.Link>
         )}
       </Nav.Item>
       <Nav.Item>
         {step3 ? (
           <LinkContainer to="/payment">
-            <Nav.Link>Payment</Nav.Link>
+            <Nav.Link>
+              <small className="mr-2">&#10104;</small>Payment
+            </Nav.Link>
           </LinkContainer>
         ) : (
-          <Nav.Link disabled>Payment</Nav.Link>
+          <Nav.Link disabled>
+            <span className="mr-2">&#10114;</span>Payment
+          </Nav.Link>
         )}
       </Nav.Item>
       <Nav.Item>
         {step4 ? (
           <LinkContainer to="/placeorder">
-            <Nav.Link>Place order</Nav.Link>
+            <Nav.Link>
+              <small className="mr-2">&#10105;</small>Place order
+            </Nav.Link>
           </LinkContainer>
         ) : (
-          <Nav.Link disabled>Place order</Nav.Link>
+          <Nav.Link disabled>
+            <span className="mr-2">&#10115;</span>Place order
+          </Nav.Link>
         )}
       </Nav.Item>
     </Nav>
